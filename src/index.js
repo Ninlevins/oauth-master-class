a41ada93ae9c73a5261f20fa6d41084fa4bda052
Ninlevins <133236290+Ninlevins@users.noreply.github.com>
2023-05-11 18:09:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,22 @@ const fetchYandexData = (token) =>
 
 window.onload = () => {
   document.getElementById("suggest").onclick = () => {
-    // TODO suggest
+    YaAuthSuggest.init({
+         client_id: "98b7e72bbc8e4e628ed46921bf86b3b0",
+         response_type: "token",
+         redirect_uri: "https://oauth-master-class-teal.vercel.app/token.html"
+      },
+      "https://oauth-master-class-teal.vercel.app/"
+   )
+    .then(({ handler }) => handler())
+      .then(async (data) => {
+        const result = await fetchYandexData(data.access_token);
+
+        authorize(result);
+
+        console.log(result, data);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
   };
   document.getElementById("button").onclick = () => {
     // TODO button
